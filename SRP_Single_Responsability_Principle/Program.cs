c1934db1279f28278a392746bce542a8fe336458
Clay Aldas <clayaldas@gmail.com>
2025-09-04 12:10:05 -0500

    Proyecto para SOLID creado

---
+++ SRP_Single_Responsability_Principle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SRP_Single_Responsability_Principle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
