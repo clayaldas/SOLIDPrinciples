5803f7dab98af696528434de89bd60aae814c5c2
Clay Aldas <clayaldas@gmail.com>
2025-09-23 09:20:30 -0500

    Agregado el principio ISP

---
+++ ISP_Interface_Segregation_Principle/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ISP_Interface_Segregation_Principle;
+
+interface IWorker
+{
+  void Work();
+  void Eat();
+  void Study();
+}
+
+class HumanWorker : IWorker
+{
+  public void Work() => Console.WriteLine("El ser humano trabaja...");
+  public void Eat() => Console.WriteLine("El ser humano come...");
+  public void Study() => Console.WriteLine("El ser humano estudia...");
+}
+
+class RobotWorker : IWorker
+{
+  public void Work() => Console.WriteLine("Un robot trabaja...");
+
+  // Métodos que no tienen sentido para un robot
+  public void Eat() => throw new NotImplementedException("Un robot no come!");
+  public void Study() => throw new NotImplementedException("Un robot no estudia!");
+}
+
+class Program
+{
+  static void Main()
+  {
+    IWorker human = new HumanWorker();
+    IWorker robot = new RobotWorker();
+
+    human.Work();
+    human.Eat();
+    human.Study();
+
+    robot.Work();
+    // Problema: el robot se ve obligado a implementar métodos inútiles
+    Console.WriteLine("\nPresione cualquier tecla para continuar...");
+    Console.ReadKey();
+  }
+}
--- LSP_Liskov_Substitution_Principle_Incorrect/obj/Debug/net8.0/LSP_Liskov_Substitution_Principle_Incorrect.AssemblyInfo.cs
+++ LSP_Liskov_Substitution_Principle_Incorrect/obj/Debug/net8.0/LSP_Liskov_Substitution_Principle_Incorrect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LSP_Liskov_Substitution_Principle_Incorrect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d5f82ecd1f5f72f0cabd8ec3071efec6a2590c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+778cb89d041d95639febd7055ccb06eb77ef76d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LSP_Liskov_Substitution_Principle_Incorrect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LSP_Liskov_Substitution_Principle_Incorrect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
